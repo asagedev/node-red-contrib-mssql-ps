0902b8255ccc6d5b728ac8c5b0ebd54872c03924
atsage <adam.sage@qualitystoneveneer.com>
2018-03-28 08:28:46 -0400

    Fixes #1. Replaced tabs with spaces.

--- mssql-ps/mssql-ps-serverconf.js
+++ mssql-ps/mssql-ps-serverconf.js
@@ -2,17 +2,17 @@ module.exports = function(RED) {
     function mssqlpsserverconf(n) {
         RED.nodes.createNode(this,n);
         this.host = n.host;
-		this.encryption = n.encryption;
-		this.database = n.database;
-		this.timeout = n.timeout;
-		this.username = n.username;
-		this.password = n.password;
+        this.encryption = n.encryption;
+        this.database = n.database;
+        this.timeout = n.timeout;
+        this.username = n.username;
+        this.password = n.password;
     }
 
-	RED.nodes.registerType("mssql-ps-serverconf", mssqlpsserverconf,{
-		credentials: {
-			username: {type:"text"},
-			password: {type:"password"}
-		}
-	});
+    RED.nodes.registerType("mssql-ps-serverconf", mssqlpsserverconf,{
+        credentials: {
+            username: {type:"text"},
+            password: {type:"password"}
+        }
+    });
 };
